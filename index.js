abb9915d6c36e340f0411f7cd27418b7e499e9c0
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-17 22:07:45 -0400

    include my first draft of the code, but commented out

--- index.js
+++ index.js
@@ -36,3 +36,30 @@ server.listen(port, (error) => {
         console.log('server listenging on port ' + port)
     }
 })
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url
+//     let fileName
+
+//     if (url === '/index') {
+//         fileName = './pages/index.html';
+//     } else if (url === '/about') {
+//         fileName = './pages/about.html';
+//     } else if (url === '/contact') {
+//         fileName = './pages/contact.html';
+//     } else if (url === '/') {
+//         fileName = './pages/index.html'; 
+//     } else {
+//         fileName = './pages/404.html';
+//     }
+
+//     fs.readFile(fileName, (error, data) => {
+//         if (error) {
+//             res.writeHead(404);
+//             res.end('Error: File not found');
+//         } else {
+//             res.writeHead(200, { 'Content-Type': 'text/html' });
+//             res.end(data);
+//         }
+//     });
+// });
