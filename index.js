d87034c98028e83bf5fcb1b16b98ad1892df1f30
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-01 21:06:00 -0400

    Create a route to serve index.html, and create a console.log to make sure the server is listening

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = express();
 const port = 8080;
 
 app.use(express.static("pages"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__infoSite + "/pages/index.html");
+});
+
+app.listen(port, (error) => {
+    if(error){
+        console.log('something went wrong')
+    } else {
+        console.log('server listenging on port ' + port)
+    }
+})
